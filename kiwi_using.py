ba404fb4724c89aca20ff3fd069decaa74e5c814
jayiuk <jayiuk987@gmail.com>
2024-03-31 04:52:42 +0900

    daf

--- kiwi_using.py
+++ kiwi_using.py
@@ -2,3 +2,12 @@ from kiwipiepy import Kiwi
 kiwi = Kiwi()
 result = kiwi.tokenize("공부하기 싫다")
 print(result)
+import pandas as pd
+train = pd.read_csv('C:/kogpt2_ft/chat_train.csv')
+train
+r = kiwi.tokenize("DB하이텍의 2011년 12월 유동비율은 얼마야")
+print(r)
+vocab = pd.read_csv('C:/kogpt2_ft/user_dic.tsv', sep = '\t', header = None)
+vocab
+vocab.to_csv('C:/kogpt2_ft/vocab.csv')
+vocab
