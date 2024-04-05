a998205f0b857e348ff3c3433e2a9f007f118a3a
jayiuk <jayiuk987@gmail.com>
2024-04-05 18:33:56 +0900

    dfafd

--- add_token.py
+++ add_token.py
@@ -1,7 +1,7 @@
 from transformers import PreTrainedTokenizerFast
 from transformers import GPT2LMHeadModel
 
-with open("C:/kogpt2_ft/list_a.txt", "r", encoding = "utf-8") as f:
+with open("C:/kogpt2_ft/new_list.txt", "r", encoding = "utf-8") as f:
     lines = f.readlines()
 
 a = [line.strip() for line in lines]
@@ -13,4 +13,4 @@ tokenizer.save_pretrained("add_new_token")
 new_tokenizer = PreTrainedTokenizerFast.from_pretrained("add_new_token")
 model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
 model.resize_token_embeddings(len(new_tokenizer))
-print(new_tokenizer.tokenize("(주)DB하이텍의 유동비율은 얼마야?"))
+print(new_tokenizer.tokenize("DB하이텍의 유동비율은 얼마야?"))
